5e64219c86e1f140bba3ed265a8f44427c05b03a
eslamandroid12345 <eslamemo457@gmail.com>
2022-08-29 16:46:03 +0200

    add dashboard

---
+++ resources/views/layouts/main-sidebar.blade.php
@@ -0,0 +1,80 @@
+<div class="container-fluid">
+    <div class="row">
+        <!-- Left Sidebar start-->
+        <div class="side-menu-fixed">
+            <div class="scrollbar side-menu-bg" style="overflow: scroll">
+                <ul class="nav navbar-nav side-menu" id="sidebarnav">
+                    <!-- menu item Dashboard-->
+                    <li>
+    <a href="#">
+        <div class="pull-left"><i class="ti-home"></i><span class="right-nav-text#</span>
+        </div>
+        <div class="clearfix"></div>
+    </a>
+</li>
+                    <!-- menu title -->
+                    <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title">الصفحه الرئيسيه </li>
+
+                    <!-- Grades-->
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#Grades-menu">
+                            <div class="pull-left"><i class="fas fa-school"></i><span
+                                    class="right-nav-t">قائمه الفنادق</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
+                        <ul id="Grades-menu" class="collapse" data-parent="#sidebarnav">
+                            <li><a href="#">الحجوزات</a></li>
+
+                        </ul>
+                    </li>
+                    <!-- classes-->
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#classes-menu">
+                            <div class="pull-left"><i class="fa fa-building"></i><span
+                                    class="right-nav-text">الفواتير</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
+                        <ul id="classes-menu" class="collapse" data-parent="#sidebarnav">
+                            <li><a href="#">فواتير الفنادق</a></li>
+                        </ul>
+                    </li>
+
+
+                    <!-- sections-->
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#sections-menu">
+                            <div class="pull-left"><i class="fas fa-chalkboard"></i><span
+                                    class="right-nav-text">الصلاحيات</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
+                        <ul id="sections-menu" class="collapse" data-parent="#sidebarnav">
+                            <li><a href="#">اضافه صلاحيه</a></li>
+                        </ul>
+                    </li>
+
+
+
+
+                    <!-- Users-->
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#Users-icon">
+                            <div class="pull-left"><i class="fas fa-users"></i><span class="right-nav-text">قائمه الادمن</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
+                        <ul id="Users-icon" class="collapse" data-parent="#sidebarnav">
+                            <li> <a href="fontawesome-icon.html">اضافه موظف</a> </li>
+                            <li> <a href="themify-icons.html">عرض الموظفين</a> </li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+
+        <!-- Left Sidebar End-->
+
+        <!--=================================
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Mail\MyTestMail;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('index', function () {
+    return view('index');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+Route::get('send-mail', function () {
+
+    $details = [
+        'title' => 'Mail from ItSolutionStuff.com',
+        'body' => 'This is for testing email using smtp'
+    ];
+
+    Mail::to('eslamemo457@gmail.com')->send(new MyTestMail($details));
+
+    dd("Email is Sent.");
+});
